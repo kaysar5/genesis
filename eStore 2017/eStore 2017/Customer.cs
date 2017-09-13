82fadb401a50317af84be0ce984026782f1753c3
kaysar5 <31867836+kaysar5@users.noreply.github.com>
2017-09-12 21:46:25 -0700

    initial code checkin
    All the class files for the project e Store

---
+++ eStore 2017/eStore 2017/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStore_2017
+{
+    #region Properties
+    /// <summary>
+    /// customer details
+    /// </summary>
+    class Customer
+    {
+        /// <summary>
+        /// Customer's identification number
+        /// </summary>
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        /// <summary>
+        /// This the customer's email address
+        /// </summary>
+        public string EmailAddress { get; set; }
+        /// <summary>
+        /// This the customer's phone number
+        /// </summary>
+        public int ContactNumber { get; set; }
+        /// <summary>
+        /// this is the customer's address
+        /// </summary>
+        public string Address { get; set; }
+# endregion
+
+
+
+    }
+}
---
+++ eStore 2017/eStore 2017/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eStore_2017
+{
+    #region Properties
+    /// <summary>
+    /// Product Details
+    /// </summary>
+    class Product
+    {
+        /// <summary>
+        /// Product's name
+        /// </summary>
+        public string ProductName { get; set; }
+        /// <summary>
+        /// Product's code number
+        /// </summary>
+        public int ItemCode { get; set; }
+        /// <summary>
+        /// Whether the item is available or not
+        /// </summary>
+        public bool InStock { get; set; }
+        /// <summary>
+        /// Manufacturing date
+        /// </summary>
+        public DateTime LaunchDate { get; set; }
+        #endregion
+
+
+    }
+}
